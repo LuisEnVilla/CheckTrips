20f7e23767f4a832b405cf7904b9ece64c379c15
LuisEnVilla <villak@hotmail.es>
2014-09-28 14:16:39 -0500

    Profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('profile');
+router.get('/:id', function(req, res) {
+	db.viaje.findById(req.params.id).populate('Funcionario_id').exec(function (err, viajedate){
+	  	if (err) res.render('error', {
+	        message: err.message,
+	        error: {}});
+	  	else res.render('profile',{viajedate:viajedate});
+	});
 });
 
 module.exports = router;
