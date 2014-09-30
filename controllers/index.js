f59b51487716caaf35fe0a6f3bab30a5c8ef9291
LuisEnVilla <villak@hotmail.es>
2014-09-30 01:21:47 -0500

    Pagina de perfil Funcionarios

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+	//Top viajes mas caros en traslado
+	db.viaje.find({$or:[{FechaInicio:{$regex:"08/"}},{FechaInicio:{$regex:"07/"}}]}
+		,'_id Tema Tipo GastoPasaje FechaInicio Origen Destino Funcionario_id').skip(1)
+		.limit(3).populate('Funcionario_id').exec(function (err, fecha){
+			if (err) res.send(500, err.message);
+			db.viaje.find({GastoPasaje : {$gt:8000}},'_id Tema CostoHospedaje ViaticosComp Tipo GastoPasaje Origen Destino Funcionario_id').limit(3).populate('Funcionario_id').exec(function (err, viajes){
+				if (err) res.send(500, err.message);
+			//viajes mas caros en viaticos
+				db.viaje.find({ViaticosComp : {$gt:5000}},'_id Tema CostoHospedaje ViaticosComp Tipo GastoPasaje Origen Destino Funcionario_id').limit(3).populate('Funcionario_id').exec(function (err, viajes2){
+					if (err) res.send(500, err.message);
+					db.unidad.find({CounViajes : {$gt:1}},'_id Nombre CounViajes CountFuncionarios').exec(function (err, unidades){
+						if (err) res.send(500, err.message);
+						res.render('index',{fecha:fecha, viajes:viajes, viajes2:viajes2,unidades:unidades});
+					});
+				});
+			});
+		});
 });
 
 module.exports = router;
