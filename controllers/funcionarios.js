eef737d757c40e8e6dc3d37958c52f619407f2eb
LuisEnVilla <villak@hotmail.es>
2014-09-30 00:28:52 -0500

    Pagina de perfil Funcionarios

--- controllers/funcionarios.js
+++ controllers/funcionarios.js
@@ -7,11 +7,93 @@ router.get('/:id', function(req, res) {
 	  	if (err) res.render('error', {
 	        message: err.message,
 	        error: {}});
-	  	db.viaje.count({Funcionario_id: req.params.id},function (err, contador){
+	  	db.viaje.find({Funcionario_id : req.params.id},'_id GastoPasaje CostoHospedaje FechaInicio FechaFin GastosViaticos Tema Destino',function (err, viajes){
 	  		if (err) res.render('error', {
 	        message: err.message,
 	        error: {}});
-	  		else res.render('funcionarios',{Funcionario:Funcionario,contador:contador});
+	  		var categorias = [];
+	  		var serie = [{
+		  			type : 'column',
+		  			name : 'Pasaje',
+		  			data :[],
+		  			color:'#03a9f4'
+	  			},
+	  			{
+		  			type :'column',
+		  			name : 'Hospedaje',
+		  			data : [],
+		  			color: '#FFC107'
+		  		},
+	  			{
+	  				type : 'column',
+	  				name : 'Viaticos',
+	  				data : [],
+	  				color : 'rgba(0,0,0,0.6)'
+	  			},
+	  			{
+	  				type : 'spline',
+	  				name : 'Total por Viaje',
+	  				data : [],
+	  				marker: {
+		                lineWidth: 2,
+		                lineColor: 'orange',
+		                fillColor: 'white'
+		            }
+	  			},
+	  			{
+	  				type : 'pie',
+	  				name : 'Gasto total',
+	  				data : [{
+	  					name : 'Pasaje',
+	  					y : 0,
+	  					color: '#03a9f4'
+	  				},
+	  				{
+	  					name : 'Hospedaje',
+	  					y : 0,
+	  					color: '#FFC107'
+	  				},{
+	  					name : 'Viaticos',
+	  					y : 0,
+	  					color: 'rgba(0,0,0,0.6)'
+	  				}],
+	  				center : [100,80],
+	  				size : 100,
+	  				showInLegend: false,
+		            dataLabels: {
+		                enabled: false
+		            }
+	  			}
+	  		];
+	  		var suma = 0;
+	  		var sumapasaje = 0;
+	  		var sumahospedaje = 0;
+	  		var sumaviaticos = 0;
+	  		for (var x in viajes){
+	  			categorias.push("'"+viajes[x].FechaInicio +" - "+viajes[x].FechaFin+"'");
+	  			serie[0].data.push(viajes[x].GastoPasaje);
+	  			serie[1].data.push(viajes[x].CostoHospedaje);
+	  			serie[2].data.push(viajes[x].GastosViaticos);
+	  			serie[3].data.push(viajes[x].GastoPasaje + viajes[x].CostoHospedaje + viajes[x].GastosViaticos);
+	  			suma += viajes[x].GastoPasaje + viajes[x].CostoHospedaje + viajes[x].GastosViaticos;
+	  			sumapasaje += viajes[x].GastoPasaje;
+	  			sumahospedaje += viajes[x].CostoHospedaje;
+	  			sumaviaticos += viajes[x].GastosViaticos;
+
+	  		}
+	  		serie[4].data[0].y = sumapasaje;
+	  		serie[4].data[1].y = sumahospedaje;
+	  		serie[4].data[2].y = sumahospedaje;
+	  		var highcharts = {
+	  			title :{ text : 'Gastos de viajes'},
+	  			xAxis: { categories: categorias },
+	  			labels: { items: [{ html: '$'+suma ,style: { left: '50px',top: '18px', color: 'black'}}]},
+	  			series: serie
+	  		};
+	  		var grafica = "{title :{ text : 'Gastos de viajes'},xAxis: { categories: ["+categorias.toString()+"] },labels: { items: [{ html: '"+highcharts.labels.items[0].html+"' ,style: { left: '100px',top: '18px', color: 'black'}}]},series: [{type : 'column',name : 'Pasaje',data :["+serie[0].data.toString()+"],color:'#03a9f4'},{type :'column',name : 'Hospedaje',data : ["+serie[1].data.toString()+"],color: '#FFC107'},{type : 'column',name : 'Viaticos',data : ["+serie[2].data.toString()+"],color : 'rgba(0,0,0,0.6)'},{type : 'spline',name : 'Total por Viaje',data : ["+serie[3].data.toString()+"],marker: {lineWidth: 2,lineColor: 'orange',fillColor: 'white'}},{type : 'pie',name : 'Gasto total',data : [{name : 'Pasaje',y : "+serie[4].data[0].y.toString()+",color: '#03a9f4'},{name : 'Hospedaje',y : "+serie[4].data[1].y.toString()+",color: '#FFC107'},{name : 'Viaticos',y : "+serie[4].data[2].y.toString()+",color: 'rgba(0,0,0,0.6)'}],center : [20,0],size : 100,showInLegend: false,dataLabels: {enabled: false}}]}";
+	  		//res.status(200).jsonp(grafica);
+	  		//res.status(200).jsonp({Funcionario:Funcionario, viajes:viajes});
+	  		res.render('funcionarios',{Funcionario:Funcionario,viajes:viajes,grafica:grafica});
 	  	});
 	});
 });
--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -94,4 +94,67 @@ $(function () {
             ]
         }]
     });
+    $('#container').highcharts({
+        title: {
+            text: 'Combination chart'
+        },
+        xAxis: {
+            categories: ['01/05/2014 - 02/06/2014']
+        },
+        labels: {
+            items: [{
+                html: ' consumption',
+                style: {
+                    left: '50px',
+                    top: '18px',
+                    color:'black'
+                }
+            }]
+        },
+        series: [{
+            type: 'column',
+            name: 'Jane',
+            data: [3],
+            color:'#03a9f4'
+        }, {
+            type: 'column',
+            name: 'John',
+            data: [2]
+        }, {
+            type: 'column',
+            name: 'Joe',
+            data: [4]
+        }, {
+            type: 'spline',
+            name: 'Total por viaje',
+            data: [5],
+            marker: {
+                lineWidth: 2,
+                lineColor:'orange',
+                fillColor: 'white'
+            }
+        }, {
+            type: 'pie',
+            name: 'Total consumption',
+            data: [{
+                name: 'Jane',
+                y: 13,
+                color:'#03a9f4' // Jane's color
+            }, {
+                name: 'John',
+                y: 23,
+                color: Highcharts.getOptions().colors[1] // John's color
+            }, {
+                name: 'Joe',
+                y: 19,
+                color:'rgba(0,0,0,0.6)' // Joe's color
+            }],
+            center: [100, 80],
+            size: 100,
+            showInLegend: false,
+            dataLabels: {
+                enabled: false
+            }
+        }]
+    });
 });
