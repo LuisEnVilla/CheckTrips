40558b2a7de174725305ab474bea01afd669a391
Axel Ramiro <axel.ramiro.92@gmail.com>
2014-10-02 02:38:24 -0500

    Avance

--- public/javascripts/button.js
+++ public/javascripts/button.js
@@ -1,11 +1,56 @@
 $(function(){
+	
 	var v = $('.social');
+	
 	$('#trigger').click(function() {
-		v.first().toggle(200, function nextBtn() {
-			$(this).next('.social').toggle('slow', nextBtn);
-		});
+		v.first().fadeToggle(200, function nextBtn() {
+			$(this).next('.social').fadeToggle('slow', nextBtn)
+		})
 	})
+	
 	v.click(function() {
-		v.toggle('slow')
-	});
+		v.fadeToggle('slow')
+		$('.modal-body p:nth-child(2)').text(window.location.href)
+		switch($(this).attr('id')){
+			case 'one':
+				$('h4.modal-title').text('Compartir en Facebook')
+			break
+			case 'two':
+				$('h4.modal-title').text('Compartir en Twitter')
+			break
+			case 'three':
+				$('h4.modal-title').text('Compartir en Google Plus')
+			break
+		}
+	})
+
+	$('#compare-btn').click(function() {
+		console.log('click')
+		$('#compare-card').slideToggle(400)
+	})
+
+	$('.compare-icon').click(function() {
+		var cont = $('#compare-card>.card-content')
+		
+		if (sessionStorage.data2) {
+			alert('Ya se han elegido dos elementos a comparar')
+			return
+		}
+
+		if (sessionStorage.data1) {
+			if (sessionStorage.data1!==$(this).next('.flat-btn').attr('href')){
+				sessionStorage.setItem("data2", $(this).next('.flat-btn').attr('href'))
+				cont.append('<p>' + sessionStorage.data2 + '</p>')
+				$(this).toggleClass('green')
+			return
+			}else {
+				alert('El elemento a comparar es el mismo')
+			}
+		} 
+		sessionStorage.setItem("data1", $(this).next('.flat-btn').attr('href'))
+	$(this).toggleClass('.green')
+		cont.append('<p>' + sessionStorage.data1 + '</p>')
+	})
+
+
 })
