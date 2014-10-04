5d589530d5dc36d1975ff68bb6c3fca7a8b83b89
Axel Ramiro <axel.ramiro.92@gmail.com>
2014-10-04 00:46:41 -0500

    Aclaraciones

--- public/javascripts/button.js
+++ public/javascripts/button.js
@@ -10,36 +10,31 @@ $(function(){
 	
 	v.click(function() {
 		v.fadeToggle('slow')
-		$('.modal-body p:nth-child(2)').text(window.location.href)
-		switch($(this).attr('id')){
-			case 'one':
-				$('h4.modal-title').text('Compartir en Facebook')
-			break
-			case 'two':
-				$('h4.modal-title').text('Compartir en Twitter')
-			break
-			case 'three':
-				$('h4.modal-title').text('Compartir en Google Plus')
-			break
-		}
 	})
 
 	$('#compare-btn').click(function() {
-		console.log('click')
 		$('#compare-card').slideToggle(400)
+		$(this).toggleClass('green')
 	})
 
+	var cont = $('#compare-card>.card-content')
+
 	$('.compare-icon').click(function() {
-		var cont = $('#compare-card>.card-content')
-		
+		 $( this ).toggleClass( "green" )
+		 console.log($(this).attr('class'))
+		 $( this ).toggleClass( "green" )
+		 console.log($(this).attr('class'))
+		var aidi = $(this).next('.flat-btn').attr('href').split('/')[2]
+		console.log(aidi);
+
 		if (sessionStorage.data2) {
 			alert('Ya se han elegido dos elementos a comparar')
 			return
 		}
 
 		if (sessionStorage.data1) {
-			if (sessionStorage.data1!==$(this).next('.flat-btn').attr('href')){
-				sessionStorage.setItem("data2", $(this).next('.flat-btn').attr('href'))
+			if (sessionStorage.data1!==aidi){
+				sessionStorage.setItem("data2", aidi)
 				cont.append('<p>' + sessionStorage.data2 + '</p>')
 				$(this).toggleClass('green')
 			return
@@ -47,10 +42,16 @@ $(function(){
 				alert('El elemento a comparar es el mismo')
 			}
 		} 
-		sessionStorage.setItem("data1", $(this).next('.flat-btn').attr('href'))
-	$(this).toggleClass('.green')
+		sessionStorage.setItem("data1", aidi)
 		cont.append('<p>' + sessionStorage.data1 + '</p>')
 	})
 
+	$('#limpiar').click(function(event) {
+		sessionStorage.clear()
+		cont.remove()
+	});
 
+	$('#comparar').click(function(event) {
+		window.location.assign('/compare/'+'2013.I.0013'+'/'+'2013.N.0013')
+	});
 })
