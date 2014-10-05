646c7395cbb148fed9cada67e916ba6c622bfd1d
Axel Ramiro <axel.ramiro.92@gmail.com>
2014-10-05 01:45:01 -0500

    Comparar funcionarios

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var Funcionarios = require('./controllers/funcionarios');
 var seguir = require('./controllers/agregarSeguir');
 var aclaracion = require('./controllers/aclaraciones');
 var api = require('./controllers/api');
+var search = require('./controllers/search');
+
 var app = express();
 // Configuration
 
@@ -42,6 +44,7 @@ app.use('/insert', insert);
 app.use('/seguir',seguir);
 app.use('/aclarar',aclaracion);
 app.use('/api',api);
+app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
