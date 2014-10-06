31a815de0a65bd4031ae513a39ef5c212fea6274
LuisEnVilla <villak@hotmail.es>
2014-10-05 20:52:31 -0500

    Busquedas

--- controllers/search.js
+++ controllers/search.js
@@ -11,7 +11,7 @@ router.get('/top', function(req, res) {
 				if (err) res.send(500, err.message);
 				db.viaje.find({$or:[{GastosViaticos:{$type:16}},{GastosViaticos:{$type:18}},{GastosViaticos:{$type:1}}]},'Consecutivo Aclaraciones _id Tema  GastoPasaje FechaInicio Origen Destino Funcionario_id').populate('Funcionario_id').sort({GastosViaticos : -1}).limit(4).exec(function (err, viajes2){
 					if (err) res.send(500, err.message);
-					res.render('search',{fecha:fecha, viajes:viajes, viajes2:viajes2,unidades:unidades});
+					res.render('search',{fecha:fecha, viajes:viajes, viajes2:viajes2});
 				});
 			});
 		});
@@ -21,13 +21,28 @@ router.get('/viajes', function(req, res) {
 		'Consecutivo Aclaraciones _id Tema  GastoPasaje FechaInicio Origen Destino Funcionario_id')
 		.populate('Funcionario_id').exec(function (err, viajes){
 			if (err) res.send(500, err.message);
-			res.render('search',{viajes:viajes});			
+			res.render('viajes',{viajes:viajes});			
 		});
 });
 
+router.post('/viajes',(function(req,res){
+	db.viaje.find({Consecutivo : req.body.buscar},'Consecutivo Aclaraciones _id Tema  GastoPasaje FechaInicio Origen Destino Funcionario_id')
+	.populate('Funcionario_id').exec(function (err, viajes){
+			if (err) res.send(500, err.message);
+			res.render('viajes',{viajes:viajes});			
+		});
+}));
+
 router.get('/funcionarios', function(req, res) {
 	db.funcionario.find({Viajes:{$exists:true}}).sort({"Nombre.Nombres" : 1}).populate('Viajes').exec((function(err,aux){
-		res.render('search',{funcionarios:aux});
+		res.render('searchfun',{funcionarios:aux});
+		//res.send(aux);
+	}));
+});
+
+router.post('/funcionarios', function(req, res) {
+	db.funcionario.find({'Nombre.Nombres':req.body.buscar}).sort({"Nombre.Nombres" : 1}).populate('Viajes').exec((function(err,aux){
+		res.render('searchfun',{funcionarios:aux});
 		//res.send(aux);
 	}));
 });
@@ -35,8 +50,17 @@ router.get('/funcionarios', function(req, res) {
 router.get('/dependencias', function(req, res) {
 	db.unidad.find().sort({Nombre : 1}).populate('Viajes').exec(function (err, unidades){
 			if (err) res.send(500, err.message);
-			res.send(unidades);
-			//res.render('search',{unidades:unidades});			
+			//res.send(unidades);
+			res.render('dependencias',{dependencias:unidades});			
+		});
+});
+
+router.post('/dependencias', function(req, res) {
+	db.unidad.find({Nombre :{$regex:req.body.buscar}}).sort({Nombre : 1}).populate('Viajes').exec(function (err, unidades){
+			if (err) res.send(500, err.message);
+
+			//res.send(unidades);
+			res.render('dependencias',{dependencias:unidades});			
 		});
 });
 
