992f2f65d7b5a3bdcfaade799948ce23206a373b
Axel Ramiro <axel.ramiro.92@gmail.com>
2014-10-19 21:51:03 -0500

    Routes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,22 @@
 (function(){
-var app = angular.module("main", []);
+var app = angular.module("main", ['ngRoute']);
+	app.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/details/:id', {
+        templateUrl: '../views/details.html',
+      }).
+      when('/search', {
+        templateUrl: '../views/search.html'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+      $locationProvider.html5Mode({
+      	enabled : true,
+      	requireBase: false
+      });
+  }]);
 	app.factory('tabService', function(){
 		var tab = 1;
 		return {
@@ -12,6 +29,18 @@ var app = angular.module("main", []);
 			}
 		}
 	})
+	app.factory('tripService', ['$http', function($http){
+		return {
+			trip : function(id){
+				$http.get('/viajes/' + id ).success(function(data){
+					return data
+				})
+			}
+		}
+	}])
+	app.controller('detailsCtrl', ['$scope','$routeParams','tripService', function($scope,$routeParams,tripService){
+		$scope.trip = tripService.trip($routeParams.id);
+	}])
 	app.controller("tripsCtrl", ['$http', '$scope', 'tabService' , (function($http, $scope, tabService){
 		$scope.isSelected = function(checkTab){
 			return tabService.isSelected(checkTab)
@@ -47,6 +76,18 @@ var app = angular.module("main", []);
 		return {
 			restrict: 'E',
 			templateUrl : '../views/func-card.html'
+		}
+	});
+	app.directive('appHeader', function(){
+		return {
+			restrict : 'E',
+			templateUrl : '../views/app-header.html'
+		}
+	});
+	app.directive('tabs', function(){
+		return {
+			restrict : 'E',
+			templateUrl : '../views/tabs.html'
 		}
 	});
 })();
