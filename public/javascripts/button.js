030b46494e8a3034d1a654f7e42a9549b3df085c
Axel Ramiro <axel.ramiro.92@gmail.com>
2014-10-27 09:13:30 -0600

    --

--- public/javascripts/button.js
+++ public/javascripts/button.js
@@ -146,5 +146,24 @@ $(function(){
 			alert('Elija dos elementos a comparar')
 		}
 	})
+	var overlay = $('.sidebar-overlay');
+
+    $('.sidebar-toggle').on('click', function() {
+    	console.log('clicki');
+        var sidebar = $('#sidebar');
+        sidebar.toggleClass('open');
+        if ((sidebar.hasClass('sidebar-fixed-left') || sidebar.hasClass('sidebar-fixed-right')) && sidebar.hasClass('open')) {
+            overlay.addClass('active');
+        } else {
+            overlay.removeClass('active');
+        }
+    });
+
+    overlay.on('click', function() {
+        $(this).removeClass('active');
+        $('#sidebar').removeClass('open');
+    });
+    console.log(overlay);
+
 
 })
